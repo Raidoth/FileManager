65c60d7ffcfbf79a3dae88ac23d81b122e78b6f0
Raidoth <49725244+Raidoth@users.noreply.github.com>
2022-03-06 19:55:00 +0300

    Update lib_app.cpp

--- dll/lib_app.cpp
+++ dll/lib_app.cpp
@@ -5,7 +5,7 @@ BOOL IsAppRunningAsAdminMode()
 	DWORD dwError = ERROR_SUCCESS;
 	PSID pAdministratorsGroup = NULL;
 
-	// Allocate and initialize a SID of the administrators group.
+
 
 	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
 	if (!AllocateAndInitializeSid(
@@ -19,9 +19,7 @@ BOOL IsAppRunningAsAdminMode()
 		dwError = GetLastError();
 	}
 
-	// Determine whether the SID of administrators group is enabled in
-
-	// the primary access token of the process.
+	
 
 	if (!CheckTokenMembership(NULL, pAdministratorsGroup, &fIsRunAsAdmin))
 	{
@@ -29,7 +27,7 @@ BOOL IsAppRunningAsAdminMode()
 	
 	}
 
-	// Centralized cleanup for all allocated resources.
+	
 
 	if (pAdministratorsGroup)
 	{
@@ -37,7 +35,7 @@ BOOL IsAppRunningAsAdminMode()
 		pAdministratorsGroup = NULL;
 	}
 
-	// Throw the error if something failed in the function.
+	
 
 	if (ERROR_SUCCESS != dwError)
 	{
